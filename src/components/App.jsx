cc03ac1bbef9d0d766ccefbbe5a86ac1d0da30c8
Krystian Guz <krystian.guz@outlook.com>
2023-03-09 22:26:22 +0100

    quick fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,9 @@ import css from './App.module.css';
 import { Profile } from './Profile/Profile';
 
 import user from '../data/user.json';
-import data from '../data/data.json';
-import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
+//import data from '../data/data.json';
+//import friends from '../data/friends.json';
+//import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
